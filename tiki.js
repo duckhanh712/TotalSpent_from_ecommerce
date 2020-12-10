987ff702a9c623f99e5c49f5a399cd00e14407de
duckhanh <duckhanh97pt@gmail.com>
2020-12-11 00:21:52 +0700

    first cm

---
+++ tiki.js
@@ -0,0 +1,51 @@
+var totalOrders = 0;
+var totalSpent = 0;
+var pulling = true;
+var page = 0;
+
+function getStatistics() {
+	var orders = [];
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			orders = JSON.parse(this.responseText)['data'];
+			totalOrders += orders.length;
+            if(orders.length >=10) {
+                pulling = orders.length
+            }else{
+                pulling = false;
+            }
+			orders.map(order => {
+				let tpa = order["grand_total"];
+				totalSpent += tpa;
+			});
+			page += 10;
+			console.log('đã lấy: ' + totalOrders + ' đơn hàng');
+			if(pulling) {
+				console.log('loading...');
+				getStatistics();
+			}
+			else {
+			
+                console.log("%cTổng đơn hàng thành công: "+"%c "+moneyFormat(totalOrders), "font-size: 20px;","font-size: 20px; color:red");
+				console.log("%cTổng chi tiêu: "+"%c "+moneyFormat(totalSpent)+"đ","font-size: 20px;","font-size: 20px; color:red");
+                console.log("%cfb: duckhanh0712","font-size: 20px; color:blue");
+			}
+		}
+	};
+	xhttp.open("GET", "https://tiki.vn/api/v2/me/orders?page="+page+"&limit=10", true);
+	xhttp.send();
+}
+
+function moneyFormat(number, fixed=0) {
+	if(isNaN(number)) return 0;
+	number = number.toFixed(fixed);
+	let delimeter = ',';
+	number += '';
+	let rgx = /(\d+)(\d{3})/;
+	while (rgx.test(number)) {
+		number = number.replace(rgx, '$1' + delimeter + '$2');
+	}
+	return number;
+}
+getStatistics()
